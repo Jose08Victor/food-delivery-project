c06f3c8aa70c720ec48c0d33b605c8ad5bd03566
José Victor <jose08.victor@gmail.com>
2024-06-19 21:02:07 -0300

    feat: "header" component added.

---
+++ client/src/components/header/index.jsx
@@ -0,0 +1,15 @@
+import './styles.css'
+
+export const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, at rerum, animi voluptatum quia quidem ut corrupti odio dignissimos eligendi iste amet provident! Quidem quasi a eum minus laborum similique too blue.</p>
+
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
