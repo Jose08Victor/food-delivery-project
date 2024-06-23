979c7307aacc76d077e493d110ee772d3d4d165b
José Victor <jose08.victor@gmail.com>
2024-06-23 18:46:30 -0300

    feat:  LoginPopup and footer component added.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,34 @@
-import NavBar from "./components/navBar"
+import { NavBar } from "./components/navBar"
+import { Route, Routes } from 'react-router-dom'
+import { Home } from "./pages/home"
+import { Cart } from "./pages/cart"
+import { PlaceOrder } from "./pages/placeOrder"
+import { Footer } from "./components/footer"
+import { useState } from "react"
+import { LoginPopUp } from "./components/loginPopUP"
+
 
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
-    <div className="app">
-      <NavBar />
-    </div>
+    <>
+      {showLogin ? <LoginPopUp setShowLogin={setShowLogin} /> : <></>}
+      <div className="app">
+        <NavBar setShowLogin={setShowLogin} />
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+
+          <Route path="/cart" element={<Cart />} />
+
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+
+      <Footer />
+    </>
   )
 }
 
